62148a3be12dc0152de4e086f7f690e3302271a3
lhrb <lukas.block@protonmail.com>
2020-11-16 20:07:30 +0100

    Clean up namespace

--- src/cpxlyze/core.clj
+++ src/cpxlyze/core.clj
@@ -19,74 +19,32 @@
    [cheshire.core :as json])
   (:gen-class))
 
-(def path "../vega/")
+(defn word-cloud [log]
+  (oz/view! (get-cloud log)
+           :mode :vega))
 
-(def log (get-log path "2019-01-01"))
-(def loc (get-loc path))
+(defn hotspots-viz [stratified]
+ (oz/view! (get-viz-description stratified)
+           :mode
+           :vega))
 
-(build-data loc log)
+(defn knowledge-viz [stratified]
+ (oz/view! (-> stratified
+               (add-main-author-name)
+               (get-knowledge-dist))
+           :mode
+           :vega))
 
-(oz/view! (get-cloud log)
-          :mode :vega)
+(defn coupling-viz [log]
+  (let [nodes (sum-of-coupling log)
+        links (link-data nodes (change-coupling-links log))]
+    (oz/view! (get-coupling-viz nodes links)
+              :mode
+              :vega)))
 
-(oz/start-server!)
-
-(oz/view! (get-viz-description (prepare-for-stratify (build-data loc log)))
-          :mode
-          :vega)
-
-(oz/view! (-> (build-data loc log)
-              (prepare-for-stratify)
-              (add-main-author-name)
-              (get-knowledge-dist))
-          :mode
-          :vega)
-
-
-(->> (prepare-for-stratify log loc)
-     (remove #(nil? (:revisions %)))
-     (filter #(>= (:revisions %) 0.25)))
-
-(oz/view! {:data {:name "test"
-                  :values (->> (prepare-for-stratify log loc)
-                              (remove #(nil? (:revisions %)))
-                              (map #(select-keys % [:revisions])))}
-           :mark "bar"
-           :encoding {:x {:field "revisions" :bin {:binned true
-                                              :step 0.05}}
-                      :y {:aggregate "count"}}})
-
-
-
-
-(def nodes (sum-of-coupling log))
-(def links (link-data nodes (change-coupling-links log)))
-
-(->> links
-     (filter #(or (= (:source %) 1) (= (:target %) 1))))
-
-
-(oz/view! (get-coupling-viz nodes links) :mode :vega)
-
-(oz/view!
- [:div
-  [:h1 "toller titel"]
-  [:vega
-     (get-coupling-viz nodes links)]
-  ])
-;; [:h1 "zweiter"]
-;;  [:vega (get-viz-description (prepare-for-stratify (build-data loc log)))]
-
-
-(sprint-interval "2020-11-11" "2019-01-01")
-
-(let [data (sprint-summaries
-                    (group-by-sprint
-                     (sprint-interval "2020-11-11" "2019-01-01")
-                     log))]
+(defn sprint-summary-viz [data]
   (oz/view!
    [:div
-
     [:vega-lite
      {:width 800
       :data {:values data}
@@ -111,6 +69,38 @@
       :mark "line"
       :encoding {:y {:field "entities" :type "quantitative"}
                  :x {:field "sprint" :type "temporal"}}}]]))
+
+
+(comment
+  (def path "../vega/")
+
+  (def log (get-log path "2019-01-01"))
+  (def loc (get-loc path))
+
+  (oz/start-server!)
+
+  (let [stratified (prepare-for-stratify (build-data loc log))]
+                                        ;(hotspots-viz stratified)
+                                        ;(knowledge-viz stratified)
+    )
+
+  (sprint-summary-viz (->> log
+                           (group-by-sprint (sprint-interval "2020-11-11" "2019-01-01"))
+                           (sprint-summaries)))
+
+  (let [last-sprint (->> log
+                         (group-by-sprint
+                          (sprint-interval "2020-11-11" "2019-01-01"))
+                         last
+                         :data)
+        stratified (prepare-for-stratify (build-data loc last-sprint))]
+    (hotspots-viz stratified)
+    ;(knowledge-viz stratified)
+    ;(word-cloud last-sprint)
+    ;(coupling-viz last-sprint)
+    )
+
+ )
 
 
 (defn -main
